d085c18f42ab2b913c1abae5e82b20de2fd49563
curlymary <mariaab74@yandex.ru>
2020-04-16 13:56:23 +0500

    Xml view

--- app/src/main/java/com/example/git_project/MainActivity.kt
+++ app/src/main/java/com/example/git_project/MainActivity.kt
@@ -3,24 +3,34 @@ package com.example.git_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), PricePrinterView, ListPrinterView {
-
-    val presenter = ProductsPresenter(this, this)
+class MainActivity : AppCompatActivity() //, PricePrinterView, ListPrinterView
+{
+    //val presenter = ProductsPresenter(this, this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        presenter.totalPricePrint()
-        presenter.productListPrint()
+        txtTotalSum.text = "2000Р"
+        txtDiscountSum.text = "300Р"
+        txtCostSum.text = "2300Р"
+
+        btnOrder.setOnClickListener{
+            Toast.makeText(this,"test", Toast.LENGTH_SHORT).show()
+        }
+
+        //presenter.totalPricePrint()
+        //presenter.productListPrint()
     }
 
-    override fun print(price: Double) {
+    /*override fun print(price: Double) {
         Log.d("Print", "Price: $price")
     }
 
     override fun printList(productList: List<Product>) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    }*/
 }
